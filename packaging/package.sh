86fd1e0c767712e4bd16c0bbb89709ce5d2a6119
Ivan Shapovalov <intelfx100@gmail.com>
2012-10-13 14:55:44 +0400

    Packager: remove leading "v" from the tag to produce version for makepkg.

--- packaging/package.sh
+++ packaging/package.sh
@@ -8,7 +8,8 @@ VERSION_VAR="NAW_VERSION"
 MD5_VAR="NAW_MD5"
 
 [[ -z "$TAG" ]] && { TAG="unspecified"; }
-NAME="netcfg-auto-wireless-$TAG"
+VERSION="${TAG#v}"
+NAME="netcfg-auto-wireless-$VERSION"
 
 echo "==== Packaging script version $TAG" >&2
 
@@ -17,6 +18,6 @@ install -Dm644 PKGBUILD "$DESTDIR/PKGBUILD"
 tar -cf "$DESTDIR/$NAME.tar" --exclude-vcs --exclude-backups --exclude=packaging --transform "s|^|$NAME/|" ../*
 xz -9e "$DESTDIR/$NAME.tar"
 MD5=$(md5sum -b "$DESTDIR/$NAME.tar"* | cut -d' ' -f1)
-sed -r -e "s|\%$VERSION_VAR\%|$TAG|" -e "s|\%$MD5_VAR\%|$MD5|" -i "$DESTDIR/PKGBUILD"
+sed -r -e "s|\%$VERSION_VAR\%|$VERSION|" -e "s|\%$MD5_VAR\%|$MD5|" -i "$DESTDIR/PKGBUILD"
 
 echo "==== Packaging done" >&2
