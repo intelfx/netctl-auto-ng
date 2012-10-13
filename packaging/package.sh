3a56614f1e95d739ceb03bce21ef760d5aab4cbf
Ivan Shapovalov <intelfx100@gmail.com>
2012-10-13 14:51:04 +0400

    Add template PKGBUILD and packaging script.

---
+++ packaging/package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DESTDIR="$(realpath $DESTDIR)"
+[[ "$DESTDIR" ]] || { echo "DESTDIR unset, exiting"; exit 1; }
+
+TAG="$(git describe)"
+VERSION_VAR="NAW_VERSION"
+MD5_VAR="NAW_MD5"
+
+[[ -z "$TAG" ]] && { TAG="unspecified"; }
+NAME="netcfg-auto-wireless-$TAG"
+
+echo "==== Packaging script version $TAG" >&2
+
+rm -rf "$DESTDIR"
+install -Dm644 PKGBUILD "$DESTDIR/PKGBUILD"
+tar -cf "$DESTDIR/$NAME.tar" --exclude-vcs --exclude-backups --exclude=packaging --transform "s|^|$NAME/|" ../*
+xz -9e "$DESTDIR/$NAME.tar"
+MD5=$(md5sum -b "$DESTDIR/$NAME.tar"* | cut -d' ' -f1)
+sed -r -e "s|\%$VERSION_VAR\%|$TAG|" -e "s|\%$MD5_VAR\%|$MD5|" -i "$DESTDIR/PKGBUILD"
+
+echo "==== Packaging done" >&2
