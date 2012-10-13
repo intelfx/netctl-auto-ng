ddb648902e763cef3368788f3e79050601234c6d
Ivan Shapovalov <intelfx100@gmail.com>
2012-10-13 11:53:54 +0400

    Add versioning to the installer.

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 DESTDIR="$(realpath $DESTDIR)"
+TAG="$(git describe)"
+VERSION_VAR="NAW_VERSION"
+
+[[ -z "$TAG" ]] && { TAG="unspecified"; }
+
+echo "==== Installing script version $TAG" >&2
 
 cd source
 while read file; do
+	echo "- $file" >&2
+	rm -f "$DESTDIR/$file"
 	install -D "$file" "$DESTDIR/$file"
+	sed -re "s|^${VERSION_VAR}=.*$|${VERSION_VAR}=\"${TAG}\"|" -i "$DESTDIR/$file"
 done < <( find . -type f )
 cd ..
+
+echo "==== Installation done" >&2
